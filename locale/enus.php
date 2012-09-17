2a2a45dbe165afdac88f3cfe771cb8b523778864
arturo182 <arturo182@tlen.pl>
2012-09-17 18:40:32 +0200

    Added locale support.

---
+++ locale/enus.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'plugins' => array(
+		'analytics' => array(
+			'tracking_id' => 'Tracking ID',
+			'track_subdomains' => 'Track subdomains',
+			'main_domain' => 'Main domain',
+			'multiple domains' => 'Multiple domains'
+		)
+	)
+);
+?>
---
+++ locale/plpl.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'plugins' => array(
+		'analytics' => array(
+			'tracking_id' => 'ID śledzenia',
+			'track_subdomains' => 'Śledź subdomeny',
+			'main_domain' => 'Główna domena',
+			'multiple domains' => 'Wiele domen'
+		)
+	)
+);
+?>
